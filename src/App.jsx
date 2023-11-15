9a21814a43071bf2863ec8bd28495e35f0a1ce36
Kovalchuk63 <vov4uk4763@gmail.com>
2023-11-15 16:04:56 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,51 +3,9 @@ import { Phonebook } from 'components/Phonebook/Phonebook';
 import { Filter } from 'components/Filter/Filter';
 import { ContactsList } from 'components/Contacts/Contacts';
 import { Layout, TitleH1, TitleH2 } from 'Layout';
-import { nanoid } from 'nanoid';
-
-const getContactsLs = () => {
-  const contactsLs = localStorage.getItem('contacts');
-  if (contactsLs !== null) {
-    return JSON.parse(contactsLs);
-  }
-  return [];
-};
 
 export const App = () => {
-  const [contacts, setContacts] = useState(getContactsLs);
-  const [filter, setFilter] = useState('');
-
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
-  const addContact = newContact => {
-    const isContactExists = contacts.some(
-      contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
-    );
-
-    if (isContactExists) {
-      alert(`Контакт з іменем ${newContact.name} вже існує.`);
-      return;
-    }
-
-    setContacts(prevContacts => [
-      ...prevContacts,
-      { ...newContact, id: nanoid },
-    ]);
-  };
-
-  const filterValue = target => {
-    setFilter(target);
-  };
-
-  const deleteContact = id => {
-    setContacts(prevState => prevState.filter(contact => contact.id !== id));
-  };
-
-  const visibleItems = contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
+  const contacts = useSelector(getContacts);
   return (
     <React.Fragment>
       <Layout>
