a6b9851888eb8f9b2ff9c1e92efa38def83f224f
Kovalchuk63 <vov4uk4763@gmail.com>
2023-11-15 16:21:19 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -3,18 +3,23 @@ import ReactDOM from 'react-dom/client';
 import { GlobalStyle } from 'GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import { App } from 'App';
+import { persistor, store } from 'redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
 
 const theme = {
-  bgColorSection: '#a9a9a9',
+  bgColorSection: '#30D4B9',
   radius: '5px',
   spacing: value => `${value * 4}px`,
 };
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-      <GlobalStyle />
-    </ThemeProvider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <ThemeProvider theme={theme}>
+        <App />
+        <GlobalStyle />
+      </ThemeProvider>
+    </PersistGate>
+  </Provider>
 );
