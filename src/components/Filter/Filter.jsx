9c759bc1448f65533fc4b04fcbd4d2b601248784
Kovalchuk63 <vov4uk4763@gmail.com>
2023-11-15 15:59:33 +0200

    update filer

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Input } from './Filter.styled';
-export const Filter = ({ inputValue, onFilter }) => {
+import { useDispatch, useSelector } from 'react-redux';
+import { filterValue } from 'redux/filterSlice';
+import { getFilter } from 'redux/selectors';
+export const Filter = () => {
+  const filter = useSelector(getFilter);
+  const dispath = useDispatch();
   return (
     <React.Fragment>
       <label htmlFor="filter">Find contacts by name</label>
       <Input
         id="filter"
-        value={inputValue}
-        onChange={evt => onFilter(evt.target.value)}
+        value={filter}
+        onChange={evt => dispath(filterValue(evt.target.value))}
       ></Input>
     </React.Fragment>
   );
